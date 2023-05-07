ee1b9fc0be01f2d5a861ff2f1c292316fa3fcf38
Donald Evans <devans08123@gmail.com>
2023-05-07 13:59:03 -0400

    mongo demo

---
+++ models/user.js
@@ -0,0 +1,25 @@
+// Username, age, location
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    location: {
+        type: String
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 13
+    }
+
+})
+
+module.exports = mongoose.model('User', userSchema)
